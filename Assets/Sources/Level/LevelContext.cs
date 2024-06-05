1333e641e07a8cb6a5843b2f4fff1497318a8283
Vladisvell <73733747+Vladisvell@users.noreply.github.com>
2024-06-05 20:24:01 +0500

    Adjustments 2 (#46)

--- Assets/Sources/Level/LevelContext.cs
+++ Assets/Sources/Level/LevelContext.cs
@@ -3,7 +3,7 @@ using System.Collections.ObjectModel;
 public class LevelContext
 {
     public string Id => ListIds[Index];
-    public bool IsLast => Index >= ListIds.Count;
+    public bool IsLast => Index >= ListIds.Count - 1;
     public int Index { get; private set; }
     public ReadOnlyCollection<string> ListIds { get; private set; }
     public string FromId { get; private set; }
